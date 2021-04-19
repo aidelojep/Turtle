94eed16b44b4df74a4e682905e55ab24ccfc19c0
aidelojep <aidelojepeter123@gmail.com>
2021-04-19 08:05:53 +0100

    Turtle has a beautiful stepping direction.

---
+++ src/Tdd/Direction.java
@@ -0,0 +1,8 @@
+package Tdd;
+
+public enum Direction {
+    EAST, SOUTH, WEST, NORTH;
+
+    public void increaseColumnPositionBy(int numberOfSteps) {
+    }
+}
---
+++ src/Tdd/Pen.java
@@ -0,0 +1,13 @@
+package Tdd;
+
+public class Pen {
+    private boolean isDown;
+
+    public void setIsUp(boolean isUp) {
+        isDown = !isUp;
+    }
+
+    public boolean isUp() {
+        return !isDown;
+    }
+}
